b8a97dbfdc91fe5a793ea64747a63f1fcb02e225
MichaaeellA <48150106+MichaaeellA@users.noreply.github.com>
2025-12-29 20:53:57 +0300

    Update numberChecker-solution.spec.js

--- foundations/03_numberChecker/solution/numberChecker-solution.spec.js
+++ foundations/03_numberChecker/solution/numberChecker-solution.spec.js
@@ -1,8 +1,8 @@
 const numberChecker = require('./numberChecker-solution')
 
 describe('numberChecker', () => {
-  test('returns true when number is 1000', () => {
-    expect(numberChecker(1000)).toEqual(true);
+  test('returns true when number is 6', () => {
+    expect(numberChecker(6)).toEqual(true);
   });
   test('returns true when number is 10', () => {
     expect(numberChecker(10)).toEqual(true);
